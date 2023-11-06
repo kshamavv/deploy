487d29d089ab2939ff819811d6cec2664eb7e8ef
kshamavv <kshama.venkatesh@66degrees.com>
2023-11-06 19:16:35 +0530

    editedd

--- main.py
+++ main.py
@@ -59,49 +59,50 @@ def rfm_func():
     rfm['RFM_Score'] = rfm['R_Score'] + rfm['F_Score'] + rfm['M_Score']
     rfm['CustomerID'] =rfm['CustomerID'].astype("int64")
 
-    final_df= rfm.merge(str_df,on='CustomerID',how='inner')
-
-    return "Rfm analysis completed"
+    # final_df= rfm.merge(str_df,on='CustomerID',how='inner')
     client = bigquery.Client()
 
-table_id = "prj-gradient-kshama.kshama.newww"
+    table_id = "prj-gradient-kshama.kshama.newww"
 
-job_config = bigquery.LoadJobConfig(
-    schema=[
-        bigquery.SchemaField("No","INT64"),
-        bigquery.SchemaField("Unnamed","INT64"),
-        bigquery.SchemaField("CustomerID", "INT64"),
-        bigquery.SchemaField("UnitPrice", "FLOAT64"),
-        bigquery.SchemaField("InvoiceDate", "STRING"),
-	bigquery.SchemaField("InvoiceNo", "INT64"),
-    bigquery.SchemaField("Recency","INT64"),
-	bigquery.SchemaField("TotalAmount", "FLOAT64"),
-	bigquery.SchemaField("R_Score", "INT64"),
-	bigquery.SchemaField("F_Score", "INT64"),
-	bigquery.SchemaField("M_Score", "INT64"),
-	bigquery.SchemaField("RFM_Group", "INT64"),
-	bigquery.SchemaField("RFM_Score", "INT64"),
-	bigquery.SchemaField("Description", "STRING"),
-	bigquery.SchemaField("Country", "STRING")
+    job_config = bigquery.LoadJobConfig(
+        schema=[
+            bigquery.SchemaField("No","INT64"),
+            bigquery.SchemaField("Unnamed","INT64"),
+            bigquery.SchemaField("CustomerID", "INT64"),
+            bigquery.SchemaField("UnitPrice", "FLOAT64"),
+            bigquery.SchemaField("InvoiceDate", "STRING"),
+        bigquery.SchemaField("InvoiceNo", "INT64"),
+        bigquery.SchemaField("Recency","INT64"),
+        bigquery.SchemaField("TotalAmount", "FLOAT64"),
+        bigquery.SchemaField("R_Score", "INT64"),
+        bigquery.SchemaField("F_Score", "INT64"),
+        bigquery.SchemaField("M_Score", "INT64"),
+        bigquery.SchemaField("RFM_Group", "INT64"),
+        bigquery.SchemaField("RFM_Score", "INT64"),
+        bigquery.SchemaField("Description", "STRING"),
+        bigquery.SchemaField("Country", "STRING")
 
-    ],
-    skip_leading_rows=1,
-    # time_partitioning=bigquery.TimePartitioning(
-    #     type_=bigquery.TimePartitioningType.DAY,
-    #     field="date",  # Name of the column to use for partitioning.
-    #     expiration_ms=7776000000,  # 90 days.
-    # ),
-)
-uri = "gs://appdeployee/updated.csv"  
+        ],
+        skip_leading_rows=1,
+        # time_partitioning=bigquery.TimePartitioning(
+        #     type_=bigquery.TimePartitioningType.DAY,
+        #     field="date",  # Name of the column to use for partitioning.
+        #     expiration_ms=7776000000,  # 90 days.
+        # ),
+    )
+    uri = "gs://appdeployee/updated.csv"  
 
-load_job = client.load_table_from_uri(
-    uri, table_id, job_config=job_config
-)  # Make an API request.
+    load_job = client.load_table_from_uri(
+        uri, table_id, job_config=job_config
+    )  # Make an API request.
 
-load_job.result()  
+    load_job.result()  
 
-table = client.get_table("prj-gradient-kshama.kshama.newww")
-print("Loaded {} rows to table {}".format(table.num_rows,"prj-gradient-kshama.kshama.newww"))  #table_id
+    table = client.get_table("prj-gradient-kshama.kshama.newww")
+    print("Loaded {} rows to table {}".format(table.num_rows,"prj-gradient-kshama.kshama.newww")) 
+
+    return "Rfm analysis completed"
+     #table_id
 if __name__ == "__main__":
     app.debug = True
     app.host='0.0.0.0'
